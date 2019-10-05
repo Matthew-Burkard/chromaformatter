d1ea1738434c0bcce8c36e0bb7bc68e3afca3d0e
mjb <matthewjburkard@outlook.com>
2019-10-05 09:45:42 -0400

    Updated description, version and classifiers in setup.py.

--- setup.py
+++ setup.py
@@ -2,15 +2,18 @@ from setuptools import setup
 
 setup(
     name='chromalogging',
-    version='1.0',
+    version='0.0.1',
     url='https://gitlab.com/mburkard/chroma-logging',
     license='MIT',
     author='Matthew Burkard',
     author_email='matthewjburkard@gmail.com',
-    description='Python logging module with colored output.',
+    description='Python logging module wrapper with colored output.',
     package_dir={'': 'src'},
     packages=['chromalogging'],
-    long_description=open('README.rst').read(),
-    zip_safe=False,
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ],
     install_requires=['sty']
 )
