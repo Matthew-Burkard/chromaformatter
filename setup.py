27855e10e7b0c701e8a6470b5c9110affc58b6af
mjb <matthewjburkard@outlook.com>
2019-11-16 15:45:20 -0500

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chromalogging',
-    version='0.0.7',
+    version='0.0.8',
     url='https://gitlab.com/mburkard/chroma-logging',
     license='MIT',
     author='Matthew Burkard',
