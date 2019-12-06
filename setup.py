65af24906e9cac748246366f02c98b83ec2da14b
mjb <matthewjburkard@outlook.com>
2019-12-05 19:44:31 -0500

    Fixed typo in version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chromalogging',
-    version='0.09',
+    version='0.0.9',
     url='https://gitlab.com/mburkard/chroma-logging',
     license='MIT',
     author='Matthew Burkard',
