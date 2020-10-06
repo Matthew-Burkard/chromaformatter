1bdedfdfd6a8b76fe45625a31a222ad3b8ee5275
mburkard <matthewjburkard@gmail.com>
2020-10-05 20:27:12 -0400

    - Switched to GPL-3 and added license headers.

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+# Copyright © 2020 Matthew Burkard
+#
+# This file is part of Chroma Logging.
+#
+# Chroma Logging is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Chroma Logging is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Chroma Logging.  If not, see <https://www.gnu.org/licenses/>.
+
 from setuptools import setup
 
 setup(
@@ -14,7 +31,7 @@ setup(
     long_description=open('README.md').read(),
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License'
+        'License :: OSI Approved :: GPL-3 License'
     ],
     install_requires=['colorama'],
     zip_safe=False
--- test/tests.py
+++ test/tests.py
@@ -1,3 +1,20 @@
+# Copyright © 2020 Matthew Burkard
+#
+# This file is part of Chroma Logging.
+#
+# Chroma Logging is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Chroma Logging is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Chroma Logging.  If not, see <https://www.gnu.org/licenses/>.
+
 import re
 import sys
 from pathlib import Path
